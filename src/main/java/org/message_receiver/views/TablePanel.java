bbf01595defdce4771d09f06b140545b11ee04f2
al-fian <st118436@alumni.ait.ac.th>
2022-12-07 20:37:40 +0700

    add popup menu

--- src/main/java/org/message_receiver/views/TablePanel.java
+++ src/main/java/org/message_receiver/views/TablePanel.java
@@ -5,6 +5,8 @@ import org.message_receiver.models.PersonTableModel;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.List;
 
 
@@ -12,11 +14,25 @@ public class TablePanel extends JPanel {
 
     private final JTable _table;
     private final PersonTableModel _personTableModel;
+    private final JPopupMenu _popup;
 
     public TablePanel() {
 
         _personTableModel = new PersonTableModel();
         _table = new JTable(_personTableModel);
+        _popup = new JPopupMenu();
+
+        JMenuItem _removeMenuItem = new JMenuItem("Delete row");
+        _popup.add(_removeMenuItem);
+
+        _table.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                if (e.getButton() == MouseEvent.BUTTON3) {
+                    _popup.show(_table, e.getX(), e.getY());
+                }
+            }
+        });
 
         setLayout(new BorderLayout());
         add(new JScrollPane(_table), BorderLayout.CENTER);
